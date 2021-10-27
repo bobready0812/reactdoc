0f98f2566240706a1dbcf3037e8c6ae50277d735
DaehyeonKim <Daehyeonoff@gmail.com>
2021-10-27 17:17:11 +0900

    20211027

--- src/component/Calc.js
+++ src/component/Calc.js
@@ -1,12 +1,24 @@
 import { useState } from "react"
+import { Fragment } from "react/cjs/react.production.min";
 
 export default function Calc() {
-    const [user, SetUser] = useState('');
-    const [com, SetCom] = useState('');
+    const [user, setUser] = useState('');
+    const [com, setCom] = useState('');
+    const [result,setResult] = useState('');
     
 
     return (
-     
+     <Fragment>
+     <div>
+         <h1>I choose: {user} </h1>
+         <h1>com choose: {com} </h1>
+         <h1>Who's the winner? : {result}</h1>
+     </div>
+
+   
+
+
+     </Fragment>
     )
 
 }
