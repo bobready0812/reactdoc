b081b39a9a56f4a2e2fa73b80521f910cb886a4e
DaehyeonKim <Daehyeonoff@gmail.com>
2021-10-27 17:11:38 +0900

    20211027

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css' ;
-import Nav from './component/Nav';
+import Nav from './component/Calc';
 
 function App() {
   return (
-    <Nav />
+    <Calc />
     
   );
 }
---
+++ src/component/Calc.js
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+export default function Calc() {
+    const [user, SetUser] = useState('');
+    const [com, SetCom] = useState('');
+    
+
+    return (
+     
+    )
+
+}
